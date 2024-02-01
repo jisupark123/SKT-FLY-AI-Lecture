d6491269cff551b7bbd34d6d75050aa041d8841e
박지수 <jisupark2000@gmail.com>
2024-02-01 10:39:54 +0900

    1/30 공부 내용.

---
+++ week5/chatbot_mysql/config/databaseConfig.py
@@ -0,0 +1,8 @@
+DB_HOST = "172.23.251.203"
+DB_USER = "root"
+DB_PASSWORD = "apptools"
+DB_NAME = "flyai"
+
+
+def DatabaseConfig():
+    global DB_HOST, DB_USER, DB_PASSWORD, DB_NAME
---
+++ week5/chatbot_mysql/train_tools/dict/create_dict.py
@@ -0,0 +1,45 @@
+import sys
+import os
+
+sys.path.append(os.path.join(os.getcwd(), "week5/chatbot_mysql"))
+
+from utils.preprocess import Preprocess
+from tensorflow import keras
+from keras import preprocessing
+import pickle
+
+
+# 말뭉치 데이터 읽어오기
+def read_corpus_data(filename):
+    with open(filename, "r") as f:
+        data = [line.split("\t") for line in f.read().splitlines()]
+        data = data[1:]  # 헤더 제거
+    return data
+
+
+# 말뭉치 데이터 가져오기
+corpus_data = read_corpus_data(
+    "/Users/my_home/Documents/Python/skt_fly_ai/week5/chatbot_mysql/train_tools/dict/corpus.txt"
+)
+
+# 말뭉치 데이터에서 키워드만 추출해서 사전 리스트 생성
+p = Preprocess()
+dict = []
+for c in corpus_data:
+    pos = p.pos(c[1])
+    for k in pos:
+        dict.append(k[0])
+
+# 사전에 사용될 단어 인덱스 딕셔너리(word_index) 생성
+tokenizer = preprocessing.text.Tokenizer(oov_token="OOV")
+tokenizer.fit_on_texts(dict)
+word_index = tokenizer.word_index
+
+# 사전 파일 생성
+f = open("chatbot_dict.bin", "wb")
+try:
+    pickle.dump(word_index, f)
+except Exception as e:
+    print(e)
+finally:
+    f.close()
